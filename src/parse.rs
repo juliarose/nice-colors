6165e2f8166b3d7c543db3b5497158fee1a9a7b6
juliarose <julialunarose@gmail.com>
2024-06-27 04:59:24 -0400

    Organize functions better

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,51 @@
 use crate::color::{SLICE_LENGTH, Alpha, Value};
 use crate::helpers::{self, conversions};
 
+/// Attempts to parse a hexadecimal color string into a color.
+pub fn hex(mut hex: &str, must_include_hash: bool) -> Option<[u8; SLICE_LENGTH]> {
+    let mut len = hex.len();
+    
+    if hex.starts_with('#') {
+        hex = &hex[1..len];
+        len -= 1;
+    } else if must_include_hash {
+        return None;
+    }
+    
+    if !matches!(len, 3 | 4 | 6 | 8) {
+        return None;
+    }
+    
+    let decimal = u32::from_str_radix(hex, 16).ok()?;
+    
+    return match len {
+        3 => Some([
+            (((decimal >> 8) & 0xF) * 0x11) as Value, // Red
+            (((decimal >> 4) & 0xF) * 0x11) as Value, // Green
+            ((decimal & 0xF) * 0x11) as Value, // Blue
+        ]),
+        4 => Some([
+            (((decimal >> 12) & 0xF) * 0x11) as Value, // Red
+            (((decimal >> 8) & 0xF) * 0x11) as Value, // Green
+            (((decimal >> 4) & 0xF) * 0x11) as Value, // Blue
+            // Skip alpha
+        ]),
+        6 => Some([
+            ((decimal >> 16) & 0xFF) as Value, // Red
+            ((decimal >> 8) & 0xFF) as Value, // Green
+            (decimal & 0xFF) as Value, // Blue
+        ]),
+        8 => Some([
+            ((decimal >> 24) & 0xFF) as Value, // Red
+            ((decimal >> 16) & 0xFF) as Value, // Green
+            ((decimal >> 8) & 0xFF) as Value, // Blue
+            // Skip alpha
+        ]),
+        // Never actually reached with the "matches" check above
+        _ => None,
+    };
+}
+
 /// Converts an HSL color string to a slice of R, G, B color values as u8 integers.
 pub fn hsl(mut hsl: &str) -> Option<([u8; SLICE_LENGTH], Alpha)> {
     let mut len = hsl.len();
@@ -82,51 +127,6 @@ pub fn hsl(mut hsl: &str) -> Option<([u8; SLICE_LENGTH], Alpha)> {
     let b = helpers::float_to_value(conversions::hue_to_rgb(m1, m2, hue - 1.0 / 3.0) * 255.0);
     
     Some(([r, g, b], alpha))
-}
-
-/// Attempts to parse a hexadecimal color string into a color.
-pub fn hex(mut hex: &str, must_include_hash: bool) -> Option<[u8; SLICE_LENGTH]> {
-    let mut len = hex.len();
-    
-    if hex.starts_with('#') {
-        hex = &hex[1..len];
-        len -= 1;
-    } else if must_include_hash {
-        return None;
-    }
-    
-    if !matches!(len, 3 | 4 | 6 | 8) {
-        return None;
-    }
-    
-    let decimal = u32::from_str_radix(hex, 16).ok()?;
-    
-    return match len {
-        3 => Some([
-            (((decimal >> 8) & 0xF) * 0x11) as Value, // Red
-            (((decimal >> 4) & 0xF) * 0x11) as Value, // Green
-            ((decimal & 0xF) * 0x11) as Value, // Blue
-        ]),
-        4 => Some([
-            (((decimal >> 12) & 0xF) * 0x11) as Value, // Red
-            (((decimal >> 8) & 0xF) * 0x11) as Value, // Green
-            (((decimal >> 4) & 0xF) * 0x11) as Value, // Blue
-            // Skip alpha
-        ]),
-        6 => Some([
-            ((decimal >> 16) & 0xFF) as Value, // Red
-            ((decimal >> 8) & 0xFF) as Value, // Green
-            (decimal & 0xFF) as Value, // Blue
-        ]),
-        8 => Some([
-            ((decimal >> 24) & 0xFF) as Value, // Red
-            ((decimal >> 16) & 0xFF) as Value, // Green
-            ((decimal >> 8) & 0xFF) as Value, // Blue
-            // Skip alpha
-        ]),
-        // Never actually reached with the "matches" check above
-        _ => None,
-    };
 }
 
 /// Attempts to parse an rgb or rgba color string into a color. Alpha value defaults to `1.0` if 
